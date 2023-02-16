8aa7a6bb86506d7b8ad7faabccc4d755ad27c8c5
faithoputa <faithoputa>
2023-02-16 10:26:22 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studiod.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle, followed by new line"
+ * return 0.
+ */
+
+int main(void)
+
+{
+ 	puts("\"programming is like building a multilingual puzzle");
+
+	return (0);
+}	
