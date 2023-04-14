3c577470c9dc53c099df1fca36211683d070ac66
faithoputa <faithoputa>
2023-04-14 15:11:56 -0700

    file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,42 +8,42 @@
 #define WRITE_ERR "Error: Can't write to %s\n"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for Alx  School students.
  * @argc: num of args
  * @argv: args
  * Return: Always 0.
  */
 int main(int argc, char **argv)
 {
-	int from, to, on_close, y, x;
+	int src_fd, dest_fd, close_status, read_bytes, write_bytes;
 	char buffer[1024];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (to == -1)
+	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (dest_fd == -1)
 		dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
-	from = open(argv[1], O_RDONLY);
-	if (from == -1)
+	src_fd = open(argv[1], O_RDONLY);
+	if (src_fd == -1)
 		dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
 	while (1)
 	{
-		x = read(from, buffer, 1024);
-		if (x == -1)
+		read_bytes = read(src_fd, buffer, 1024);
+		if (read_bytes == -1)
 			dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
-		if (x > 0)
+		if (read_bytes > 0)
 		{
-			y = write(to, buffer, x);
-			if (y == -1)
+			write_bytes = write(dest_fd, buffer, read_bytes);
+			if (write_bytes == -1)
 				dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
 		} else
 			break;
 	}
-	on_close = close(from);
-	if (on_close == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close su %d\n", from), exit(100);
-	on_close = close(to);
-	if (on_close == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close su %d\n", to), exit(100);
+	close_status = close(src_fd);
+	if (close_status == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src_fd), exit(100);
+	close_status = close(dest_fd);
+	if (close_status == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd), exit(100);
 	return (0);
 }
